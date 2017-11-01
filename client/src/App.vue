f66bc4263dab79853ff6c5c5e1b5dd1edd8a57dc
leopen <790480953@qq.com>
2017-11-02 07:58:02 +0800

    [leopen] first commit

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="example">
+  <h1>{{ msg }}</h1>
+  <ul>
+    <li v-for="(item, index) in list" v-bind:key="index">{{item}}</li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hello Vue!",
+      list: [1, 2, 3, 4]
+    };
+  }
+};
+</script>
+
+<style scoped>
+#example {
+  color: red;
+  height: 100vh;
+}
+</style>
